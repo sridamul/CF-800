ee040ccaa5c39ced2a0bb08078ff7af39fd4cf68
Sridhar S <107404972+sridamul@users.noreply.github.com>
2023-12-24 23:25:55 +0530

    Add files via upload

---
+++ IchihimeandTriangle/ichihime.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void solve()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << b << " " << c << " " << c << "\n";
+    return;
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
